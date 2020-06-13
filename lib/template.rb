e918407d6de30b3461ddf4bda6f7186ba60cffe3
ian-1 <51008396ian-1@users.noreply.github.com>
2020-06-13 17:22:13 +0100

    update test to first real template and pass

--- lib/template.rb
+++ lib/template.rb
@@ -1,9 +1,17 @@
 class Template
   def see_templates
-    '1) Test template'
+    '1) Return holding deposit - offer not formally accepted'
   end
 
   def select
-    'Test template contents'
+    'The landlord has not formally accepted your offer. As such we will return your holding deposit to you.
+
+It is very frustrating as the landlord did previously state that they would go with your offer.
+
+I am very sorry about this. It is very frustrating as you have paid your deposit ready for the landlord to formally accept. Unfortunately without formal acceptance there is little we can do but return your money.
+
+Please can you pass me your bank details (name of account holder, account number and sort code) and I will ensure that your holding deposit is transferred back to you.
+
+I wish you every luck in your property search and will certainly let you know if any other similar properties become available so that you can be the first to view.'
   end
 end
--- spec/template_spec.rb
+++ spec/template_spec.rb
@@ -6,7 +6,7 @@ describe Template do
   end
 
   it "check see_templates returns templates list" do
-    expect(subject.see_templates).to eq('1) Test template')
+    expect(subject.see_templates).to eq('1) Return holding deposit - offer not formally accepted')
   end
 
   it "check responds to select" do
@@ -14,6 +14,14 @@ describe Template do
   end
 
   it "check see_templates returns template contents" do
-    expect(subject.select).to eq('Test template contents')
+    expect(subject.select).to eq('The landlord has not formally accepted your offer. As such we will return your holding deposit to you.
+
+It is very frustrating as the landlord did previously state that they would go with your offer.
+
+I am very sorry about this. It is very frustrating as you have paid your deposit ready for the landlord to formally accept. Unfortunately without formal acceptance there is little we can do but return your money.
+
+Please can you pass me your bank details (name of account holder, account number and sort code) and I will ensure that your holding deposit is transferred back to you.
+
+I wish you every luck in your property search and will certainly let you know if any other similar properties become available so that you can be the first to view.')
   end
 end
