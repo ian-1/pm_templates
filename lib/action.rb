155999236a9d80fbf7ab63ad35b41e5db33b2962
ian-1 <51008396ian-1@users.noreply.github.com>
2020-06-16 12:01:28 +0100

    add show_list test and pass

--- lib/action.rb
+++ lib/action.rb
@@ -1,16 +1,26 @@
 class Action
   def record(user, message)
-    file = File.open("#{user}_actions.llama", 'w')
-    file.print message
+    file = File.open("#{user}_actions.llama", 'a')
+    file.puts message
     file.close
   end
 
   def read_last(user)
     file = File.open("#{user}_actions.llama", "r")
     file.readlines.each do |line|
-      @message = line
+      @message = line[0..-2]   # cuts ' \n' left by puts
     end
     file.close
     @message
+  end
+
+  def show_list(user)
+    list = []
+    file = File.open("#{user}_actions.llama", "r")
+    file.readlines.each do |line|
+      list.push(line[0..-2])
+    end
+    file.close
+    list
   end
 end
--- spec/action_spec.rb
+++ spec/action_spec.rb
@@ -5,6 +5,7 @@ describe Action do
     @user = 'test_llama'
     @message1 = 'no drama llama'
     @message2 = 'no probllama'
+    @message3 = 'Stay calmer with a llama'
   end
 
   it "check responds to record" do
@@ -15,7 +16,11 @@ describe Action do
     expect(subject).to respond_to :read_last
   end
 
-  it "check read_last returns last recorded action for user 'test'" do
+  it "check responds to show_list" do
+    expect(subject).to respond_to :show_list
+  end
+
+  it "check read_last returns last recorded action for user" do
     subject.record(@user, @message1)
     expect(subject.read_last(@user)).to eq(@message1)
   end
@@ -24,6 +29,20 @@ describe Action do
     @file_name = "#{@user}_actions.llama"
     @instruction = "echo #{@message2} > #{@file_name}"
     system(@instruction)
-    expect(subject.read_last(@user)).to eq("#{@message2} \n")
+    expect(subject.read_last(@user)).to eq("#{@message2} ")
+  end
+
+  it "check show_list returns all recorded actions for user" do
+    file = File.open("#{@user}_actions.llama", 'w')
+    file.print ''
+    file.close
+    subject.record(@user, @message1)
+    subject.record(@user, @message2)
+    subject.record(@user, @message3)
+    expected_array = []
+    expected_array.push(@message1)
+    expected_array.push(@message2)
+    expected_array.push(@message3)
+    expect(subject.show_list(@user)).to eq(expected_array)
   end
 end
